c8caee5905bdfad156c6c8de68b8c8db4f1e1bed
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2022-11-11 01:42:15 -0600

    slight change to onclick call

--- SoftwareLabProject/src/pages/components/HWSet.tsx
+++ SoftwareLabProject/src/pages/components/HWSet.tsx
@@ -27,8 +27,8 @@ class HWSet extends React.Component{
         <div className="flexbox-container">
             <div>{this.name}: {this.availability} / {this.capacity}</div>
             <div><TextField id="HWQty" label="Enter Qty" variant="outlined" /></div>
-            <div><Button color = "primary" variant = "contained" onClick={()=> {this.checkIn}}> Check In </Button></div>
-            <div><Button color = "primary" variant = "contained" onClick={()=> {this.checkOut}}> Check Out </Button></div>
+            <div><Button color = "primary" variant = "contained" onClick={this.checkIn}> Check In </Button></div>
+            <div><Button color = "primary" variant = "contained" onClick={this.checkOut}> Check Out </Button></div>
         </div>
         );
     }
