e6195371c03da92f87dd07a5d98dfc039e71377c
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2022-11-12 16:22:58 -0600

    dynamic rendering projectview

--- SoftwareLabProject/src/pages/Projects.tsx
+++ SoftwareLabProject/src/pages/Projects.tsx
@@ -7,22 +7,42 @@ import { BorderAllOutlined, FirstPage } from '@material-ui/icons';
 
 function Projects(){
     const navigate = useNavigate();
+    const username = "PLACEHOLDER"
+    const projects = [
+        {
+            name: 'Project One',
+            set_one: 100,
+            set_two: 200,
 
+        },
+        {
+            name: 'Project Two',
+            set_one: 150,
+            set_two: 300,
+        }
+    ]
    
     return(
-        <>
-        <div style = {{display:'inline-flex', float:'left', marginLeft:20}}>
-            <h1>
-                Hello Placeholder Name
-            </h1>
+        <div className='App'>
+            <div style = {{display:'inline-flex', float:'left', marginLeft:20}}>
+                <h1>
+                    Welcome {username}
+                </h1>
+            </div>
+            { projects.map(project => <Projectview key={project.name} project={project} />) }
         </div>
+    );
+}
+
+function Projectview(props: any){
+    return(<div>
         <Grid container spacing = {2} justifyContent = "flex-start" style = {{flexDirection:'row', borderBlockStyle: 'solid', height: '160px'}}>
             <Grid item xs = {4} sm = {2} style = {{margin:0}}>
-                <h2>Project Name Placeholder</h2>
+                <h2> {props.project.name}</h2>
             </Grid>
             <Grid item xs = {4} sm = {2} justifyContent = "center" style = {{flexDirection: 'column', padding:'30px'}}>
-                <p>hardware 1</p>
-                <p>hardware 2</p>
+                <p>HWSET1: {props.project.set_one}</p>
+                <p>HWSET2: {props.project.set_two}</p>
             </Grid>
             <Grid item xs = {4} sm = {2}  style = {{padding: 20}}>
                 <TextField variant = 'outlined' id = "hardwareset_1"  label = "Enter QTY" type = "number"  margin = "dense" >
@@ -44,9 +64,7 @@ function Projects(){
                 <Button variant = "contained" color = "primary" style = {{height: '40px', width:'100%'}}> LEAVE </Button>
             </Grid>
         </Grid>
-        </>
-    );
+    </div>);
 }
-
 
 export default Projects;
